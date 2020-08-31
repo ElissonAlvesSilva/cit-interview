f5c285f1addd784a4c1d33b7ec3fa318844c08df
Elisson Alves Silva <elisson@MacBook-Pro-de-Elisson.local>
2020-08-31 14:45:34 -0400

    feat: validate schedule start and end date

--- businessess/schedule/index.js
+++ businessess/schedule/index.js
@@ -1,5 +1,23 @@
 const ScheduleBusinesses = {
-  handle(arrayOfSchedules) {
+  handle(arrayOfSchedules, startDate, endDate) {
+    if (!startDate || !endDate) {
+      const error = {
+        code: 1,
+        message: 'Must be pass start and end date',
+      };
+
+      throw error;
+    }
+
+    if (!(startDate instanceof Date) || !(endDate instanceof Date)) {
+      const error = {
+        code: 2,
+        message: 'start and end date must to be type equals to date',
+      };
+
+      throw error;
+    }
+
     return arrayOfSchedules;
   },
 };
--- test/schedule/index.spec.js
+++ test/schedule/index.spec.js
@@ -4,9 +4,14 @@ const ScheduleBusinesses = require('../../businessess/schedule');
 describe('Schedule', () => {
   let sandbox;
   let jobsToRun;
+  let startDate;
+  let endDate;
 
   beforeEach(() => {
     sandbox = sinon.createSandbox();
+    startDate = new Date('2019-11-10 09:00:00');
+    endDate = new Date('2019-11-11 12:00:00');
+
     jobsToRun = [
       {
         ID: 1,
@@ -34,8 +39,22 @@ describe('Schedule', () => {
   });
 
   it('should return a array set', () => {
-    const response = ScheduleBusinesses.handle(jobsToRun);
+    const response = ScheduleBusinesses.handle(jobsToRun, startDate, endDate);
 
     expect(response).to.be.a('array');
+  });
+
+  it('should return a error if start or end date is empty', () => {
+    const schedule = ScheduleBusinesses;
+    expect(() => {
+      schedule.handle(jobsToRun);
+    }).to.throw('Must be pass start and end date');
+  });
+
+  it('should return a error if start or end date is not type date', () => {
+    const schedule = ScheduleBusinesses;
+    expect(() => {
+      schedule.handle(jobsToRun, '2020-09-09', '2020-09-08');
+    }).to.throw('start and end date must to be type equals to date');
   });
 });
