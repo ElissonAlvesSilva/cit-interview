59fc3dbceed05c2bc5d4159ee2cc4e4cb0bdbc13
Elisson Alves Silva <elisson@MacBook-Pro-de-Elisson.local>
2020-09-03 08:35:29 -0400

    refact: change string to date

--- test/formatter/schedule/index.js
+++ test/formatter/schedule/index.js
@@ -12,19 +12,19 @@ describe('Formatter', () => {
         {
           ID: 1,
           Descrição: 'Importação de arquivos de fundos',
-          'Data Máxima de conclusão': '2019-11-10 12:00:00',
+          'Data Máxima de conclusão': new Date('2019-11-10 12:00:00'),
           'Tempo estimado': '2 horas',
         },
         {
           ID: 2,
           Descrição: 'Importação de dados da Base Legada',
-          'Data Máxima de conclusão': '2019-11-11 12:00:00',
+          'Data Máxima de conclusão': new Date('2019-11-11 12:00:00'),
           'Tempo estimado': '4 horas',
         },
         {
           ID: 3,
           Descrição: 'Importação de dados de integração',
-          'Data Máxima de conclusão': '2019-11-11 08:00:00',
+          'Data Máxima de conclusão': new Date('2019-11-11 08:00:00'),
           'Tempo estimado': '6 horas',
         },
       ];
@@ -48,26 +48,26 @@ describe('Formatter', () => {
       }).to.throw('Must be pass array of schedules');
     });
 
-    it('should return a formatted response', () => {
+    it('should return a formatted response order by date desc', () => {
       const format = ScheduleFormatter.format(jobsToRun);
       const formattedResponse = [
         {
           id: 1,
           description: 'Importação de arquivos de fundos',
-          maxDate: '2019-11-10 12:00:00',
-          estimated: '2 horas',
-        },
-        {
-          id: 2,
-          description: 'Importação de dados da Base Legada',
-          maxDate: '2019-11-11 12:00:00',
-          estimated: '4 horas',
+          maxDate: new Date('2019-11-10 12:00:00'),
+          estimated: 2,
         },
         {
           id: 3,
           description: 'Importação de dados de integração',
-          maxDate: '2019-11-11 08:00:00',
-          estimated: '6 horas',
+          maxDate: new Date('2019-11-11 08:00:00'),
+          estimated: 6,
+        },
+        {
+          id: 2,
+          description: 'Importação de dados da Base Legada',
+          maxDate: new Date('2019-11-11 12:00:00'),
+          estimated: 4,
         },
       ];
 
